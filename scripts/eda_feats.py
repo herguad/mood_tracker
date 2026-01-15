a244a556281453a0dbed77b9278138dfe187da22
herguad <g.s.herrera.s@gmail.com>
2026-01-15 13:03:17 -0300

    +eda_features

--- scripts/eda_feats.py
+++ scripts/eda_feats.py
@@ -6,10 +6,31 @@ df = pd.read_csv("data/moods_features.csv")
 df.info()
 df.describe(include="all")
 
-
 #Time coverage 
-lineplot 
+df = pd.read_csv("data/moods_cleaned.csv")
+
+df["full_date"] = pd.to_datetime(df["full_date"])
+
+daily_counts = (
+    df.groupby("full_date")
+      .size()
+      .rename("entries")
+)
+
+plt.figure(figsize=(12, 4))
+plt.plot(daily_counts.index, daily_counts.values)
+
+plt.title("Mood Tracking Coverage Over Time")
+plt.xlabel("Date")
+plt.ylabel("Number of Entries")
+
+plt.tight_layout()
+plt.show()
+
 #Mood counts
 barchart
 
 #Activity frequency (SB)
+
+
+#Heatmaps 
