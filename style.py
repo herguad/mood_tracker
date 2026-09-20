305b2b4e10f32368c6924e1f77276bd13e7e79d1
herguad <g.s.herrera.s@gmail.com>
2026-09-20 18:25:25 -0300

    adjust fontsize

--- style.py
+++ style.py
@@ -5,7 +5,7 @@ def apply_custom_style():
     st.markdown("""
         <style>
         div[data-testid="stMarkdownContainer"] p {
-            font-size: 1.15rem;
+            font-size: 1.2rem;
             line-height: 1.6;
         }
         </style>
