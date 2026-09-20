eaf656d7b52a5daa776c533a43455cd94f7ec4be
herguad <g.s.herrera.s@gmail.com>
2026-09-20 18:15:57 -0300

    create style file to control font size across the dashb

---
+++ style.py
@@ -0,0 +1,12 @@
+# style.py
+import streamlit as st
+
+def apply_custom_style():
+    st.markdown("""
+        <style>
+        div[data-testid="stMarkdownContainer"] p {
+            font-size: 1.15rem;
+            line-height: 1.6;
+        }
+        </style>
+    """, unsafe_allow_html=True)
